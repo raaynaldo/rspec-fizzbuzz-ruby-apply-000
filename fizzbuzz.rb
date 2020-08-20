7133fca35043b028d70336b483fa652b538e0a12
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-20 03:45:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number == 3
+    return "Fizz"
+  elsif number == 5
+    return "Buzz"
+  elsif number == 15
+    return "FizzBuzz"
+  else
+    return nil
+  end
+end
+    
+    
